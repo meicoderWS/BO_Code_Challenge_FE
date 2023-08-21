9ecde53c71d7e556547193b5a67279c4d05512ad
Marcos Espinoza <mespinoza@waverleysoftware.com>
2023-08-21 14:35:09 -0400

    configure and create the form with mui

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,42 @@
+import { Typography, Button, Stack, TextField, Box } from '@mui/material';
+import { useState } from 'react';
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+    const [name, setName] = useState('');
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name, latitude, longitude);
+    };
+    return (
+        <Box
+            sx={{
+                padding: '2rem'
+            }}
+        >
+            <Stack direction="column" spacing={5}>
+                <Stack direction="column">
+                    <Typography variant="h3" component="h1" gutterBottom>
+                        Base Operation
+                    </Typography>
+                    <Typography variant="subtitle1">Code Challenge</Typography>
+                </Stack>
+                <Box component="form" onSubmit={handleSubmit}>
+                    <Stack spacing={2} direction="column">
+                        <TextField label="name" value={name} onChange={(e) => setName(e.target.value)} required />
+                        <TextField label="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)} required />
+                        <TextField label="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)} required />
+                    </Stack>
+                    <Stack spacing={2} direction="row" justifyContent="center">
+                        <Button variant="contained" type="submit">
+                            Add Location
+                        </Button>
+                        <Button variant="outlined">Cancel</Button>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Box>
+    );
 };
 
 export default Sidebar;
